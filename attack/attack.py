4a6f24b700d72b558520245b87fca654937b0edf
saraghazanfari <s.ghazanfari.ce@gmail.com>
2023-10-15 23:02:32 -0400

    debug

--- attack/attack.py
+++ attack/attack.py
@@ -14,11 +14,11 @@ def generate_attack(attack, eps, model, x, target, loss=nn.CrossEntropyLoss()):
         adv_image = adversary.run_standard_evaluation(x, target.long(), bs=x.shape[0])
     elif attack_method == 'PGD':
         if attack_norm == 'L2':
-            adversary = L2PGDAttack(model, loss_fn=loss, eps=eps, nb_iter=1000, eps_iter=0.01, rand_init=True,
+            adversary = L2PGDAttack(model, eps=eps, nb_iter=1000, eps_iter=0.01, rand_init=True,
                                     clip_min=0.,
                                     clip_max=1., targeted=False)
         else:
-            adversary = LinfPGDAttack(model, loss_fn=loss, eps=eps, nb_iter=50, eps_iter=0.03, rand_init=True,
+            adversary = LinfPGDAttack(model, eps=eps, nb_iter=50, eps_iter=0.03, rand_init=True,
                                       clip_min=0.,
                                       clip_max=1., targeted=False)
         adv_image = adversary.perturb(x, target.long())
