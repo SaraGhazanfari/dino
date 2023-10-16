6b98fa64f96e5af30b05e73dd1f90bae5ba15609
saraghazanfari <s.ghazanfari.ce@gmail.com>
2023-10-16 13:22:51 -0400

    debug

--- utils.py
+++ utils.py
@@ -76,6 +76,7 @@ def load_pretrained_weights(model, pretrained_weights, checkpoint_key, model_nam
         if checkpoint_key is not None and checkpoint_key in state_dict:
             print(f"Take key {checkpoint_key} in provided checkpoint dict")
             state_dict = state_dict[checkpoint_key]
+        print(state_dict)
         # remove `module.` prefix
         state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
         # remove `backbone.` prefix induced by multicrop wrapper
