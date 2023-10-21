5228a108ff4ef2ca26a488768dbfd80db02fda95
saraghazanfari <s.ghazanfari.ce@gmail.com>
2023-10-21 11:22:53 -0400

    name of distance_list changed

--- eval_knn.py
+++ eval_knn.py
@@ -183,8 +183,8 @@ def knn_classifier(train_features, train_labels, test_features, test_labels, k, 
         x = x.cuda()
         if args.attack:
             original_features = model(x).detach()
-            x_adv = generate_attack(attack=args.attack, eps=args.eps, model=dist_wrapper(model, original_features), x=x,
-                                    target=original_features) #torch.zeros(x.shape[0]).cuda()
+            x_adv = generate_attack(attack=args.attack, eps=args.eps, model=model, x=x, target=original_features)
+            # torch.zeros(x.shape[0]).cuda() dist_wrapper(model, original_features)
             features = model(x_adv).detach()
             distance_list.append(1 - cos_sim(features, original_features))
         else:
