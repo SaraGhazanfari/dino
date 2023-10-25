4d6effb784ea10755cdc6223aadff5e8a1b1abbf
saraghazanfari <s.ghazanfari.ce@gmail.com>
2023-10-25 18:16:36 -0400

    eval_knn debuged

--- eval_knn.py
+++ eval_knn.py
@@ -1,276 +1,3 @@
-# # Copyright (c) Facebook, Inc. and its affiliates.
-# #
-# # Licensed under the Apache License, Version 2.0 (the "License");
-# # you may not use this file except in compliance with the License.
-# # You may obtain a copy of the License at
-# #
-# #     http://www.apache.org/licenses/LICENSE-2.0
-# #
-# # Unless required by applicable law or agreed to in writing, software
-# # distributed under the License is distributed on an "AS IS" BASIS,
-# # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# # See the License for the specific language governing permissions and
-# # limitations under the License.
-# import os
-# import sys
-# import argparse
-#
-# import torch
-# from torch import nn
-# import torch.distributed as dist
-# import torch.backends.cudnn as cudnn
-# from torchvision import datasets
-# from torchvision import transforms as pth_transforms
-# from torchvision import models as torchvision_models
-#
-# import utils
-# import vision_transformer as vits
-# from attack.attack import generate_attack
-#
-#
-# def dist_wrapper(model, ref_features):
-#     def get_dist(x):
-#         dist = 1 - nn.CosineSimilarity(dim=1, eps=1e-6)(model(x), ref_features)
-#         return torch.stack((1 - dist, dist), dim=1)
-#
-#     return get_dist
-#
-#
-# def extract_feature_pipeline(args, model):
-#     # ============ preparing data ... ============
-#     data_loader_train, data_loader_val, dataset_train, dataset_val = get_data(args)
-#     if args.load_features:
-#         train_features = torch.load(os.path.join(args.load_features, "trainfeat.pth"))
-#         train_labels = torch.load(os.path.join(args.load_features, "trainlabels.pth"))
-#     else:
-#         print("Extracting features for train set...")
-#         train_features = extract_features(model, data_loader_train, args)
-#         train_labels = torch.tensor([s[-1] for s in dataset_train.samples]).long()
-#
-#     print(f"Data loaded with {len(dataset_train)} train and {len(dataset_val)} val imgs.")
-#
-#     print("Extracting features for val set...")
-#     test_features = extract_features(model=model, data_loader=data_loader_val, args=args, is_test=True)
-#
-#     if utils.get_rank() == 0:
-#         # train_features = nn.functional.normalize(train_features, dim=1, p=2)
-#         test_features = nn.functional.normalize(test_features, dim=1, p=2)
-#
-#
-#     test_labels = torch.tensor([s[-1] for s in dataset_val.samples]).long()
-#     # save features and labels
-#     if args.dump_features and dist.get_rank() == 0:
-#         torch.save(train_features.cpu(), os.path.join(args.dump_features, "trainfeat.pth"))
-#         torch.save(train_labels.cpu(), os.path.join(args.dump_features, "trainlabels.pth"))
-#
-#     return train_features, test_features, train_labels, test_labels
-#
-#
-# def get_model(args):
-#     # ============ building network ... ============
-#     if "vit" in args.arch:
-#         model = vits.__dict__[args.arch](patch_size=args.patch_size, num_classes=0)
-#         print(f"Model {args.arch} {args.patch_size}x{args.patch_size} built.")
-#     elif "xcit" in args.arch:
-#         model = torch.hub.load('facebookresearch/xcit:main', args.arch, num_classes=0)
-#     elif args.arch in torchvision_models.__dict__.keys():
-#         model = torchvision_models.__dict__[args.arch](num_classes=0)
-#         model.fc = nn.Identity()
-#     else:
-#         print(f"Architecture {args.arch} non supported")
-#         sys.exit(1)
-#     utils.load_pretrained_weights(model, args.pretrained_weights, args.checkpoint_key, args.arch, args.patch_size)
-#     model.cuda()
-#     return model
-#
-#
-# def get_data(args):
-#     transform = pth_transforms.Compose([
-#         pth_transforms.Resize(256, interpolation=3),
-#         pth_transforms.CenterCrop(224),
-#         pth_transforms.ToTensor(),
-#         pth_transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
-#     ])
-#     dataset_train = ReturnIndexDataset(os.path.join(args.data_path, "train"), transform=transform)
-#     dataset_val = ReturnIndexDataset(os.path.join(args.data_path, "val"), transform=transform)
-#
-#     sampler = torch.utils.data.DistributedSampler(dataset_train, shuffle=False)
-#     data_loader_train = torch.utils.data.DataLoader(
-#         dataset_train,
-#         sampler=sampler,
-#         batch_size=args.batch_size_per_gpu,
-#         num_workers=args.num_workers,
-#         pin_memory=True,
-#         drop_last=False,
-#     )
-#     data_loader_val = torch.utils.data.DataLoader(
-#         dataset_val,
-#         batch_size=args.batch_size_per_gpu,
-#         num_workers=args.num_workers,
-#         pin_memory=True,
-#         drop_last=False,
-#     )
-#     return data_loader_train, data_loader_val, dataset_train, dataset_val
-#
-#
-# def extract_features(model, data_loader, args, is_test=False, multiscale=False):
-#     metric_logger = utils.MetricLogger(delimiter="  ")
-#     features = None
-#     distance_list = list()
-#     cos_sim = nn.CosineSimilarity(dim=1, eps=1e-6)
-#     for samples, index in metric_logger.log_every(data_loader, 10):
-#         samples = samples.cuda(non_blocking=True)
-#         index = index.cuda(non_blocking=True)
-#
-#         if args.attack and is_test:
-#             original_features = model(samples).detach()
-#             samples = generate_attack(attack=args.attack, eps=args.eps, model=model, x=samples,
-#                                       target=original_features)
-#             distance_list.append(1 - cos_sim(model(samples).detach(), original_features))
-#         if multiscale:
-#             feats = utils.multi_scale(samples, model)
-#         else:
-#             feats = model(samples).clone()
-#
-#         # init storage feature matrix
-#         if dist.get_rank() == 0 and features is None:
-#             features = torch.zeros(len(data_loader.dataset), feats.shape[-1])
-#             if args.use_cuda:
-#                 features = features.cuda(non_blocking=True)
-#             print(f"Storing features into tensor of shape {features.shape}")
-#
-#         # get indexes from all processes
-#         y_all = torch.empty(dist.get_world_size(), index.size(0), dtype=index.dtype, device=index.device)
-#         y_l = list(y_all.unbind(0))
-#         y_all_reduce = torch.distributed.all_gather(y_l, index, async_op=True)
-#         y_all_reduce.wait()
-#         index_all = torch.cat(y_l)
-#
-#         # share features between processes
-#         feats_all = torch.empty(
-#             dist.get_world_size(),
-#             feats.size(0),
-#             feats.size(1),
-#             dtype=feats.dtype,
-#             device=feats.device,
-#         )
-#         output_l = list(feats_all.unbind(0))
-#         output_all_reduce = torch.distributed.all_gather(output_l, feats, async_op=True)
-#         output_all_reduce.wait()
-#
-#         # update storage feature matrix
-#         if dist.get_rank() == 0:
-#             if args.use_cuda:
-#                 features.index_copy_(0, index_all, torch.cat(output_l))
-#             else:
-#                 features.index_copy_(0, index_all.cpu(), torch.cat(output_l).cpu())
-#     if args.attack and is_test:
-#         root = args.load_features if args.load_features else args.dump_features
-#         torch.save(distance_list, os.path.join(root, f'distance_list_{args.attack}_{args.eps}.pt'))
-#     return features
-#
-#
-# @torch.no_grad()
-# def knn_classifier(train_features, train_labels, test_features, test_labels, k, args, model,
-#                    num_classes=1000):
-#     top1, top5, total = 0.0, 0.0, 0
-#     train_features = train_features.t()
-#     num_test_images, num_chunks = test_labels.shape[0], 100
-#     imgs_per_chunk = 64  # num_test_images // num_chunks
-#     retrieval_one_hot = torch.zeros(k, num_classes).to(train_features.device)
-#
-#     for idx in range(0, num_test_images, imgs_per_chunk):
-#         targets = test_labels[idx: min((idx + imgs_per_chunk), num_test_images)]
-#
-#         features = test_features[idx: min((idx + imgs_per_chunk), num_test_images), :]
-#         batch_size = targets.shape[0]
-#
-#         # calculate the dot product and compute top-k neighbors
-#         similarity = torch.mm(features, train_features)
-#         distances, indices = similarity.topk(k, largest=True, sorted=True)
-#         candidates = train_labels.view(1, -1).expand(batch_size, -1)
-#         retrieved_neighbors = torch.gather(candidates, 1, indices)
-#
-#         retrieval_one_hot.resize_(batch_size * k, num_classes).zero_()
-#         retrieval_one_hot.scatter_(1, retrieved_neighbors.view(-1, 1), 1)
-#         distances_transform = distances.clone().div_(args.temperature).exp_()
-#         probs = torch.sum(
-#             torch.mul(
-#                 retrieval_one_hot.view(batch_size, -1, num_classes),
-#                 distances_transform.view(batch_size, -1, 1),
-#             ),
-#             1,
-#         )
-#         _, predictions = probs.sort(1, True)
-#
-#         # find the predictions that match the target
-#         correct = predictions.eq(targets.data.view(-1, 1))
-#         top1 = top1 + correct.narrow(1, 0, 1).sum().item()
-#         top5 = top5 + correct.narrow(1, 0, min(5, k)).sum().item()  # top5 does not make sense if k < 5
-#         total += targets.size(0)
-#     top1 = top1 * 100.0 / total
-#     top5 = top5 * 100.0 / total
-#
-#     return top1, top5
-#
-#
-# class ReturnIndexDataset(datasets.ImageFolder):
-#     def __getitem__(self, idx):
-#         img, lab = super(ReturnIndexDataset, self).__getitem__(idx)
-#         return img, idx
-#
-#
-# if __name__ == '__main__':
-#     parser = argparse.ArgumentParser('Evaluation with weighted k-NN on ImageNet')
-#     parser.add_argument('--batch_size_per_gpu', default=128, type=int, help='Per-GPU batch-size')
-#     parser.add_argument('--nb_knn', default=[10, 20, 100, 200], nargs='+', type=int,
-#                         help='Number of NN to use. 20 is usually working the best.')
-#     parser.add_argument('--temperature', default=0.07, type=float,
-#                         help='Temperature used in the voting coefficient')
-#     parser.add_argument('--pretrained_weights', default='', type=str, help="Path to pretrained weights to evaluate.")
-#     parser.add_argument('--use_cuda', default=True, type=utils.bool_flag,
-#                         help="Should we store the features on GPU? We recommend setting this to False if you encounter OOM")
-#     parser.add_argument('--arch', default='vit_small', type=str, help='Architecture')
-#     parser.add_argument('--patch_size', default=16, type=int, help='Patch resolution of the model.')
-#     parser.add_argument("--checkpoint_key", default="teacher", type=str,
-#                         help='Key to use in the checkpoint (example: "teacher")')
-#     parser.add_argument('--dump_features', default=None,
-#                         help='Path where to save computed features, empty for no saving')
-#     parser.add_argument('--load_features', default=None, help="""If the features have
-#         already been computed, where to find them.""")
-#     parser.add_argument('--num_workers', default=10, type=int, help='Number of data loading workers per GPU.')
-#     parser.add_argument("--dist_url", default="env://", type=str, help="""url used to set up
-#         distributed training; see https://pytorch.org/docs/stable/distributed.html""")
-#     parser.add_argument("--local_rank", default=0, type=int, help="Please ignore and do not set this argument.")
-#     parser.add_argument('--data_path', default='/path/to/imagenet/', type=str)
-#
-#     parser.add_argument("--attack", default=None, type=str, help='Attack L2, Linf')
-#     parser.add_argument('--eps', default=1.0, type=float, help='Perturbation budget for attack')
-#     args = parser.parse_args()
-#
-#     utils.init_distributed_mode(args)
-#     print("git:\n  {}\n".format(utils.get_sha()))
-#     print("\n".join("%s: %s" % (k, str(v)) for k, v in sorted(dict(vars(args)).items())))
-#     cudnn.benchmark = True
-#
-#     # ============ preparing model ... ============
-#     model = get_model(args)
-#     # need to extract features ! train_features, train_labels
-#     train_features, test_features, train_labels, test_labels = extract_feature_pipeline(args, model)
-#
-#     if utils.get_rank() == 0:
-#         if args.use_cuda:
-#             train_features = train_features.cuda()
-#             test_features = test_features.cuda()
-#             train_labels = train_labels.cuda()
-#             test_labels = test_labels.cuda()
-#
-#         print("Features are ready!\nStart the k-NN classification.")
-#         for k in args.nb_knn:
-#             top1, top5 = knn_classifier(train_features, train_labels,
-#                                         test_features, test_labels, k, args, model)
-#             print(f"{k}-NN classifier result: Top1: {top1}, Top5: {top5}")
-#     dist.barrier()
 # Copyright (c) Facebook, Inc. and its affiliates.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -298,6 +25,8 @@ from torchvision import models as torchvision_models
 
 import utils
 import vision_transformer as vits
+from attack.attack import generate_attack
+from attack.general_attack import GeneralAttack
 
 
 def extract_feature_pipeline(args):
@@ -346,9 +75,9 @@ def extract_feature_pipeline(args):
 
     # ============ extract features ... ============
     print("Extracting features for train set...")
-    train_features = extract_features(model, data_loader_train, args.use_cuda)
+    train_features = extract_features(model, data_loader_train, args)
     print("Extracting features for val set...")
-    test_features = extract_features(model, data_loader_val, args.use_cuda)
+    test_features = extract_features(model, data_loader_val, args, is_test=True)
 
     if utils.get_rank() == 0:
         train_features = nn.functional.normalize(train_features, dim=1, p=2)
@@ -366,12 +95,20 @@ def extract_feature_pipeline(args):
 
 
 @torch.no_grad()
-def extract_features(model, data_loader, use_cuda=True, multiscale=False):
+def extract_features(model, data_loader, args, multiscale=False, is_test=False):
     metric_logger = utils.MetricLogger(delimiter="  ")
     features = None
+    distance_list = list()
     for samples, index in metric_logger.log_every(data_loader, 10):
         samples = samples.cuda(non_blocking=True)
         index = index.cuda(non_blocking=True)
+
+        if args.attack and is_test:
+            cos_sim = nn.CosineSimilarity(dim=1, eps=1e-6)
+            original_features = model(samples).detach()
+            samples = generate_attack(attack=args.attack, eps=args.eps, model=model, x=samples,
+                                      target=original_features)
+            distance_list.append(1 - cos_sim(model(samples).detach(), original_features))
         if multiscale:
             feats = utils.multi_scale(samples, model)
         else:
@@ -380,7 +117,7 @@ def extract_features(model, data_loader, use_cuda=True, multiscale=False):
         # init storage feature matrix
         if dist.get_rank() == 0 and features is None:
             features = torch.zeros(len(data_loader.dataset), feats.shape[-1])
-            if use_cuda:
+            if args.use_cuda:
                 features = features.cuda(non_blocking=True)
             print(f"Storing features into tensor of shape {features.shape}")
 
@@ -405,10 +142,13 @@ def extract_features(model, data_loader, use_cuda=True, multiscale=False):
 
         # update storage feature matrix
         if dist.get_rank() == 0:
-            if use_cuda:
+            if args.use_cuda:
                 features.index_copy_(0, index_all, torch.cat(output_l))
             else:
                 features.index_copy_(0, index_all.cpu(), torch.cat(output_l).cpu())
+    if args.attack and is_test:
+        root = args.load_features if args.load_features else args.dump_features
+        torch.save(distance_list, os.path.join(root, f'distance_list_{args.attack}_{args.eps}.pt'))
     return features
 
 
@@ -422,9 +162,9 @@ def knn_classifier(train_features, train_labels, test_features, test_labels, k, 
     for idx in range(0, num_test_images, imgs_per_chunk):
         # get the features for test images
         features = test_features[
-            idx : min((idx + imgs_per_chunk), num_test_images), :
-        ]
-        targets = test_labels[idx : min((idx + imgs_per_chunk), num_test_images)]
+                   idx: min((idx + imgs_per_chunk), num_test_images), :
+                   ]
+        targets = test_labels[idx: min((idx + imgs_per_chunk), num_test_images)]
         batch_size = targets.shape[0]
 
         # calculate the dot product and compute top-k neighbors
@@ -465,18 +205,18 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser('Evaluation with weighted k-NN on ImageNet')
     parser.add_argument('--batch_size_per_gpu', default=128, type=int, help='Per-GPU batch-size')
     parser.add_argument('--nb_knn', default=[10, 20, 100, 200], nargs='+', type=int,
-        help='Number of NN to use. 20 is usually working the best.')
+                        help='Number of NN to use. 20 is usually working the best.')
     parser.add_argument('--temperature', default=0.07, type=float,
-        help='Temperature used in the voting coefficient')
+                        help='Temperature used in the voting coefficient')
     parser.add_argument('--pretrained_weights', default='', type=str, help="Path to pretrained weights to evaluate.")
     parser.add_argument('--use_cuda', default=True, type=utils.bool_flag,
-        help="Should we store the features on GPU? We recommend setting this to False if you encounter OOM")
+                        help="Should we store the features on GPU? We recommend setting this to False if you encounter OOM")
     parser.add_argument('--arch', default='vit_small', type=str, help='Architecture')
     parser.add_argument('--patch_size', default=16, type=int, help='Patch resolution of the model.')
     parser.add_argument("--checkpoint_key", default="teacher", type=str,
-        help='Key to use in the checkpoint (example: "teacher")')
+                        help='Key to use in the checkpoint (example: "teacher")')
     parser.add_argument('--dump_features', default=None,
-        help='Path where to save computed features, empty for no saving')
+                        help='Path where to save computed features, empty for no saving')
     parser.add_argument('--load_features', default=None, help="""If the features have
         already been computed, where to find them.""")
     parser.add_argument('--num_workers', default=10, type=int, help='Number of data loading workers per GPU.')
@@ -510,6 +250,6 @@ if __name__ == '__main__':
         print("Features are ready!\nStart the k-NN classification.")
         for k in args.nb_knn:
             top1, top5 = knn_classifier(train_features, train_labels,
-                test_features, test_labels, k, args.temperature)
+                                        test_features, test_labels, k, args.temperature)
             print(f"{k}-NN classifier result: Top1: {top1}, Top5: {top5}")
     dist.barrier()
--- visualize_attention.py
+++ visualize_attention.py
@@ -34,6 +34,7 @@ from PIL import Image
 
 import utils
 import vision_transformer as vits
+from utils.visualization_utils import visualize_att_map
 
 
 def apply_mask(image, mask, color, alpha=0.5):
@@ -76,7 +77,7 @@ def display_instances(image, mask, fname="test", figsize=(5, 5), blur=False, con
         color = colors[i]
         _mask = mask[i]
         if blur:
-            _mask = cv2.blur(_mask,(10,10))
+            _mask = cv2.blur(_mask, (10, 10))
         # Mask
         masked_image = apply_mask(masked_image, _mask, color, alpha)
         # Mask Polygon
@@ -96,15 +97,46 @@ def display_instances(image, mask, fname="test", figsize=(5, 5), blur=False, con
     return
 
 
+def main(dataloader):
+    for idx, data in tqdm(enumerate(dataloader)):
+        if idx * self.batch_size > 5:
+            break
+        inputs = data[0].to(self.device)
+        input_embed = self.model(inputs).detach()
+        visualize_att_map(inputs.squeeze(0), img_idx=idx, model=self.dreamsim_model.extractor_list[0].model,
+                          device=self.device, patch_size=patch_size,
+                          output_dir=os.path.join(self.config.teacher_model_name, 'clean'))
+        if self.config.attack:
+            adv_inputs = self.general_attack.generate_attack(inputs, img_0=None, img_1=None, target=input_embed,
+                                                             target_model=self.dreamsim_model.embed,
+                                                             is_dist_attack=True)
+
+            adv_input_embed = self.model(adv_inputs).detach()
+
+            cos_dist = 1 - self.cos_sim(input_embed.unsqueeze(0), adv_input_embed.unsqueeze(0))
+
+            dreamsim_dist_list.append(cos_dist)
+
+            visualize_att_map(adv_inputs.squeeze(0), img_idx=idx,
+                              model=self.dreamsim_model.extractor_list[0].model,
+                              device=self.device, patch_size=patch_size,
+                              output_dir=os.path.join(self.config.teacher_model_name, 'adv'))
+    if self.config.attack:
+        torch.save(dreamsim_dist_list,
+                   f'{self.config.teacher_model_name}/distance_list_{self.config.attack}_{self.config.eps}')
+
+    logging.info('finished')
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('Visualize Self-Attention maps')
     parser.add_argument('--arch', default='vit_small', type=str,
-        choices=['vit_tiny', 'vit_small', 'vit_base'], help='Architecture (support only ViT atm).')
+                        choices=['vit_tiny', 'vit_small', 'vit_base'], help='Architecture (support only ViT atm).')
     parser.add_argument('--patch_size', default=8, type=int, help='Patch resolution of the model.')
     parser.add_argument('--pretrained_weights', default='', type=str,
-        help="Path to pretrained weights to load.")
+                        help="Path to pretrained weights to load.")
     parser.add_argument("--checkpoint_key", default="teacher", type=str,
-        help='Key to use in the checkpoint (example: "teacher")')
+                        help='Key to use in the checkpoint (example: "teacher")')
     parser.add_argument("--image_path", default=None, type=str, help="Path of the image to load.")
     parser.add_argument("--image_size", default=(480, 480), type=int, nargs="+", help="Resize image.")
     parser.add_argument('--output_dir', default='.', help='Path where to save visualizations.')
@@ -115,6 +147,7 @@ if __name__ == '__main__':
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     # build model
     model = vits.__dict__[args.arch](patch_size=args.patch_size, num_classes=0)
+    print(sum(p.numel() for p in model.parameters() if p.requires_grad))
     for p in model.parameters():
         p.requires_grad = False
     model.eval()
@@ -148,76 +181,3 @@ if __name__ == '__main__':
         else:
             print("There is no reference weights available for this model => We use random weights.")
 
-    # open image
-    if args.image_path is None:
-        # user has not specified any image - we use our own image
-        print("Please use the `--image_path` argument to indicate the path of the image you wish to visualize.")
-        print("Since no image path have been provided, we take the first image in our paper.")
-        response = requests.get("https://dl.fbaipublicfiles.com/dino/img.png")
-        img = Image.open(BytesIO(response.content))
-        img = img.convert('RGB')
-    elif os.path.isfile(args.image_path):
-        with open(args.image_path, 'rb') as f:
-            img = Image.open(f)
-            img = img.convert('RGB')
-    else:
-        print(f"Provided image path {args.image_path} is non valid.")
-        sys.exit(1)
-    transform = pth_transforms.Compose([
-        pth_transforms.Resize(args.image_size),
-        pth_transforms.ToTensor(),
-        #pth_transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
-    ])
-    img = transform(img)
-    import time
-    start = time.time()
-    adversary = L2PGDAttack(model, loss_fn=nn.MSELoss(), eps=0.5, nb_iter=100,
-                            rand_init=True, targeted=False, eps_iter=0.01, clip_min=0.0, clip_max=1.0)
-    img_adv = adversary.perturb(img.unsqueeze(0), model(img.unsqueeze(0)))
-    # print(time.time() - start)
-    print(torch.norm(model(img.unsqueeze(0))-model(img_adv), p=2))
-    img = img_adv.squeeze(0)
-
-# make the image divisible by the patch size
-    w, h = img.shape[1] - img.shape[1] % args.patch_size, img.shape[2] - img.shape[2] % args.patch_size
-    img = img[:, :w, :h].unsqueeze(0)
-
-    w_featmap = img.shape[-2] // args.patch_size
-    h_featmap = img.shape[-1] // args.patch_size
-
-    attentions = model.get_last_selfattention(img.to(device))
-
-    nh = attentions.shape[1] # number of head
-
-    # we keep only the output patch attention
-    attentions = attentions[0, :, 0, 1:].reshape(nh, -1)
-
-    if args.threshold is not None:
-        # we keep only a certain percentage of the mass
-        val, idx = torch.sort(attentions)
-        val /= torch.sum(val, dim=1, keepdim=True)
-        cumval = torch.cumsum(val, dim=1)
-        th_attn = cumval > (1 - args.threshold)
-        idx2 = torch.argsort(idx)
-        for head in range(nh):
-            th_attn[head] = th_attn[head][idx2[head]]
-        th_attn = th_attn.reshape(nh, w_featmap, h_featmap).float()
-        # interpolate
-        th_attn = nn.functional.interpolate(th_attn.unsqueeze(0), scale_factor=args.patch_size, mode="nearest")[0].cpu().numpy()
-
-    attentions = attentions.reshape(nh, w_featmap, h_featmap)
-    attentions = nn.functional.interpolate(attentions.unsqueeze(0), scale_factor=args.patch_size, mode="nearest")[0].cpu().numpy()
-
-    # save attentions heatmaps
-    os.makedirs(args.output_dir, exist_ok=True)
-    torchvision.utils.save_image(torchvision.utils.make_grid(img, normalize=True, scale_each=True), os.path.join(args.output_dir,
-                                                                                                                 "clean image/img.png"))
-    for j in range(nh):
-        fname = os.path.join(args.output_dir, "attn-head" + str(j) + ".png")
-        plt.imsave(fname=fname, arr=attentions[j], format='png')
-        print(f"{fname} saved.")
-
-    if args.threshold is not None:
-        image = skimage.io.imread(os.path.join(args.output_dir, "clean image/img.png"))
-        for j in range(nh):
-            display_instances(image, th_attn[j], fname=os.path.join(args.output_dir, "mask_th" + str(args.threshold) + "_head" + str(j) +".png"), blur=False)
