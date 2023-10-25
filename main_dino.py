da256209e98c52b9e2da6831bccad88bd7c34311
saraghazanfari <s.ghazanfari.ce@gmail.com>
2023-10-25 09:56:04 -0400

    debuging

--- main_dino.py
+++ main_dino.py
@@ -140,7 +140,7 @@ class ImageFolderEX(datasets.ImageFolder):
 
     def __getitem__(self, index):
         ImageFolderEX.index_freq_dict[index] = ImageFolderEX.index_freq_dict.get(index, 0) + 1
-        return [torch.zeros((3,224,244)), 0]
+        return [self.transform(torch.zeros((3,224,244))), 0]
         # path, label = self.imgs[index]
         # try:
         #     img = self.transform(self.loader(os.path.join(self.root, path)))
